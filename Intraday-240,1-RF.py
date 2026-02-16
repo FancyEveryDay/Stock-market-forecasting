69b138c6ed1839d3a850a78023fc445a08a3296f
FancyEveryDay <dereusnick@yahoo.com>
2026-02-16 09:53:36 -0600

    Beginning work on datasets

--- Intraday-240,1-RF.py
+++ Intraday-240,1-RF.py
@@ -20,7 +20,7 @@ constituents = {'-'.join(col.split('/')[::-1]):set(SP500_df[col].dropna())
                 for col in SP500_df.columns}
 
 constituents_train = {} 
-for test_year in range(1993,2016):
+for test_year in range(1996,2025):
     months = [str(t)+'-0'+str(m) if m<10 else str(t)+'-'+str(m) 
               for t in range(test_year-3,test_year) for m in range(1,13)]
     constituents_train[test_year] = [list(constituents[m]) for m in months]
